cf9c96dc5c6052b6298c3842f98108efa16b2b0a
Ihor Poliakov <piv2003@gmail.com>
2023-04-27 23:25:04 +0200

    creating Feedback

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from 'App';
-// import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import { GlobalStyle } from 'components/GlobalStyle';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <GlobalStyle />
+  </React.StrictMode>
+);
