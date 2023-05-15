917314f2194968b47cf28638451de6c9c46b8480
Ihor Poliakov <piv2003@gmail.com>
2023-05-15 23:03:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,3 +2,11 @@ import React, { Component } from 'react';
 import { FeedbackOptions } from 'components/Feedback';
 import { Statistics } from 'components/Statistics';
 import { Section } from 'components/Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+}
