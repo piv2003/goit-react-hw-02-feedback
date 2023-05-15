535b0ef7672de6ca2d696bfbfb2195620a378d67
Ihor Poliakov <piv2003@gmail.com>
2023-05-15 10:16:51 +0200

    Update Feedback.js

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,2 +1,18 @@
 import PropTypes from 'prop-types';
 import { ButtonList, Button } from './Feedback.styled';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ButtonList>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <Button type="button" onClick={onLeaveFeedback}>
+              {option}
+            </Button>
+          </li>
+        );
+      })}
+    </ButtonList>
+  );
+};
